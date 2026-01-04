20a30c95ddad450f31ce5a862c13700b2edbb189
Nekhebet <mordvinvolga@gmail.com>
2026-01-04 15:20:07 +0300

    Update __init__.py

--- packages/nekhebet-store/nekhebet_store/__init__.py
+++ packages/nekhebet-store/nekhebet_store/__init__.py
@@ -3,3 +3,15 @@ Nekhebet Store
 
 Infrastructure adapter for persistent storage of signed envelopes.
 """
+
+from .lmdb_repository import LMDBEventRepository, ReplayDetectedError as LMDBReplayError
+from .pg_repository import EventRepository as PGEventRepository, ReplayDetectedError as PGReplayError
+from .hybrid_repository import HybridEventRepository
+
+__all__ = [
+    "LMDBEventRepository",
+    "LMDBReplayError",
+    "PGEventRepository",
+    "PGReplayError",
+    "HybridEventRepository",
+]
